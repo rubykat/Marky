b694bc9d743c1e79a1c7440ff1b51f0494db2e00
Kathryn Andersen <perlkat@katspace.org>
2017-10-12 05:34:15 +1100

    passing all tests now

--- lib/Marky/Controller/Db.pm
+++ lib/Marky/Controller/Db.pm
@@ -17,41 +17,82 @@ Database controller for Marky
 
 use Mojo::Base 'Mojolicious::Controller';
 
+=head2 tables
+
+Display the tables
+
+=cut
 sub tables {
     my $c  = shift;
     $c->render(template=>'tables');
 }
 
+=head2 options
+
+For setting the options of the app.
+
+=cut
 sub options {
     my $c  = shift;
     $c->marky_set_options();
     $c->render(template => 'settings');
 }
 
+=head2 taglist
+
+Display the list of tags in the database.
+
+=cut
 sub taglist {
     my $c  = shift;
     $c->render(template=>'taglist');
 }
 
+=head2 tagcloud
+
+Display the tags as a tagcloud.
+
+=cut
 sub tagcloud {
     my $c  = shift;
     $c->render(template=>'tagcloud');
 }
 
+=head2 query
+
+Process a query
+
+=cut
 sub query {
     my $c  = shift;
     $c->marky_do_query();
 }
 
+=head2 tags
+
+Process a query by tags.
+
+=cut
 sub tags {
     my $c  = shift;
     $c->marky_do_query();
 }
 
+=head2 add_bookmark
+
+Add a bookmark
+
+=cut
 sub add_bookmark {
     my $c  = shift;
     $c->render(template=>'add_bookmark');
 }
+
+=head2 save_bookmark
+
+Save a bookmark.
+
+=cut
 sub save_bookmark {
     my $c  = shift;
     $c->marky_save_new_bookmark();
